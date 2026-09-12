0b5973c45d153264237e38f8d6870df1e67e7105
Akira.degitech <akira.degitech@gmail.com>
2026-09-12 17:45:46 +1000

    Feat: implement DB modesl and wire up Flask routes (#1)

--- website/forms.py
+++ website/forms.py
@@ -2,20 +2,37 @@ from flask_wtf import FlaskForm
 from wtforms.fields import TextAreaField, SubmitField, StringField, PasswordField
 from wtforms.validators import InputRequired, Length, Email, EqualTo
 
+
 # creates the login information
 class LoginForm(FlaskForm):
-    user_name=StringField("User Name", validators=[InputRequired('Enter user name')])
-    password=PasswordField("Password", validators=[InputRequired('Enter user password')])
+    email = StringField("Email address", validators=[
+        InputRequired('Enter your email address'),
+        Email('Enter a valid email address'),
+    ])
+    password = PasswordField("Password", validators=[InputRequired('Enter your password')])
     submit = SubmitField("Login")
 
- # this is the registration form
-class RegisterForm(FlaskForm):
-    user_name=StringField("User Name", validators=[InputRequired()])
-    email = StringField("Email Address", validators=[Email("Please enter a valid email")])
-    # linking two fields - password should be equal to data entered in confirm
-    password=PasswordField("Password", validators=[InputRequired(),
-                  EqualTo('confirm', message="Passwords should match")])
-    confirm = PasswordField("Confirm Password")
 
-    # submit button
+# this is the registration form
+class RegisterForm(FlaskForm):
+    first_name = StringField("First name", validators=[InputRequired('Enter your first name')])
+    surname = StringField("Surname", validators=[InputRequired('Enter your surname')])
+    email = StringField("Email address", validators=[
+        InputRequired('Enter your email address'),
+        Email('Enter a valid email address'),
+    ])
+    contact_number = StringField("Contact number", validators=[InputRequired('Enter your contact number')])
+    street_address = StringField("Street address", validators=[InputRequired('Enter your street address')])
+
+    # linking two fields - password should be equal to data entered in confirm
+    password = PasswordField("Password", validators=[
+        InputRequired('Enter a password'),
+        EqualTo('confirm', message="Passwords should match"),
+    ])
+    confirm = PasswordField("Confirm password")
+
+    # TODO(#2 Register): add a validate_email(self, field) method here that
+    # queries the User table and raises wtforms.ValidationError if the email
+    # is already registered (Issue #2 acceptance criteria).
+
     submit = SubmitField("Register")
--- website/views.py
+++ website/views.py
@@ -1,7 +1,23 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 main_bp = Blueprint('main', __name__)
 
+
 @main_bp.route('/')
 def index():
-    return '<h1>Starter code for assignment 3<h1>'
+    return render_template('index.html')
+
+
+@main_bp.route('/event/<int:event_id>')
+def event_detail(event_id):
+    return render_template('event.html')
+
+
+@main_bp.route('/create', methods=['GET', 'POST'])
+def create_event():
+    return render_template('create.html')
+
+
+@main_bp.route('/bookings')
+def bookings():
+    return render_template('bookings.html')
